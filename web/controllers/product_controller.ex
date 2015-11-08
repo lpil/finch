cbc7bcd1e446afd93fa9149f8291b2a0940ae850
Louis Pilfold <louis@lpil.uk>
2015-11-08 18:23:51 +0000

    Clean up sloppiness

--- web/controllers/product_controller.ex
+++ web/controllers/product_controller.ex
@@ -6,7 +6,6 @@ defmodule Finch.ProductController do
 
   alias Ecto.Query
   alias Finch.Product
-  alias Finch.ErrorView
 
   plug :scrub_params, "product" when action in [:create] #, :update]
 
@@ -14,7 +13,7 @@ defmodule Finch.ProductController do
     products =
       Product
       |> Query.order_by([p], [p.display_name])
-      |> Repo.all 
+      |> Repo.all
       |> Repo.preload(:bundles)
     changeset =  Product.changeset
     render conn, "index.html", products: products, changeset: changeset
@@ -25,7 +24,7 @@ defmodule Finch.ProductController do
     |> Product.changeset(params)
     |> Repo.insert
     |> case do
-      {:ok, product} ->
+      {:ok, _product} ->
         conn
         |> put_flash(:info, "Product created successfully.")
         |> redirect( to: product_path(conn, :index) )
@@ -34,10 +33,5 @@ defmodule Finch.ProductController do
         conn
         |> render "new.html", changeset: changeset
     end
-  end
-
-
-  defp get_bundle(code) do
-    Bundle |> Repo.get_by!( code: code )
   end
 end
