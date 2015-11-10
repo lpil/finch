8e49895627a67fa0ed936cf207848166f08fe002
Louis Pilfold <louis@lpil.uk>
2015-11-10 10:19:25 +0000

    Remove person table

--- priv/repo/migrations/20151106185535_init.exs
+++ priv/repo/migrations/20151106185535_init.exs
@@ -1,16 +1,7 @@
-defmodule Finch.Repo.Migrations.CreatePerson do
+defmodule Finch.Repo.Migrations.Init do
   use Ecto.Migration
 
   def change do
-    # TODO: Remove
-    create table(:people) do
-      add :name, :string
-      add :email, :string
-      add :bio, :string
-      add :number_of_pets, :integer
-      timestamps
-    end
-
     create table(:bundles) do
       add :display_name, :string
       add :code, :string
